4075efa8b919eff3d3cb01e06d6773980b7178b5
Cha_nu <101384306+cksdn43@users.noreply.github.com>
2022-12-05 19:48:33 +0900

    Delete Korean_Wave directory

--- Korean_Wave/src/Execute.java
+++
@@ -1,9 +0,0 @@
-public class Execute { // 다형성 처리 클래스입니다.
-    void show(Kdrama drama) {
-        drama.introduce();
-    }
-
-    void Genre(Kdrama drama) {
-        drama.showgenre();
-    }
-}
--- Korean_Wave/src/Kpop.java
+++
@@ -1,7 +0,0 @@
-public interface Kpop { // 3. 인터페이스
-    public void show_information();
-
-    public void input(String contend);
-
-    public void init();
-}
